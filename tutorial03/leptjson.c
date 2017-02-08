54aff6556ee6b8dec604f81e8a39321a72490035
milley <milley.gm@gmail.com>
2017-02-08 18:04:42 +0800

    commit chapter3 update

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -118,7 +118,11 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                 c->top = head;
                 return LEPT_PARSE_MISS_QUOTATION_MARK;
             default:
-                PUTC(c, ch);
+                if ((unsigned char)ch < 0x20) {
+                    c->top = head;
+                    return LEPT_PARSE_INVALID_STRING_CHAR;
+                }
+                PUTC(c, ch); 
         }
     }
 }
